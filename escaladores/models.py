865e1c553a052dd4718b05bbbca0d8b0e539a0e9
Diego_Jimenez_Ksal28 <d.nicolas.jimenez95@gmail.com>
2023-11-09 00:01:33 -0500

    Agregado modelo Nuevo_escalador y su migracion

--- escaladores/models.py
+++ escaladores/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Nuevo_escalador(models.Model):
+    nombre= models.CharField(max_length=256)
+    fecha_nacimiento= models.DateField(null=True, blank=True)
+    nivel= models.CharField(max_length=256)
+    preferencia=models.CharField(max_length=250, null=True, blank=True)
