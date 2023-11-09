366c5f3099f60e1ef62ab37a9665971f6b30f480
Diego_Jimenez_Ksal28 <d.nicolas.jimenez95@gmail.com>
2023-11-09 18:59:15 -0500

    añadido panel administrativo

--- escaladores/admin.py
+++ escaladores/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from escaladores.models import Nuevo_escalador, nueva_ruta, nuevo_bloque
+
+admin.site.register(Nuevo_escalador)
+admin.site.register(nueva_ruta)
+admin.site.register(nuevo_bloque)
+
