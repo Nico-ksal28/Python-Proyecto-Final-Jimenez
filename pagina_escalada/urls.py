264ce2edf0cddf7f27b4149bc31cf7ba602f3fd0
Diego_Jimenez_Ksal28 <d.nicolas.jimenez95@gmail.com>
2023-11-24 01:12:44 -0500

    added avatar

--- pagina_escalada/urls.py
+++ pagina_escalada/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from pagina_escalada.views import *
 
@@ -29,3 +32,4 @@ urlpatterns = [
     path("", saludar_con_html, name="inicio"),
 ]
     
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- perfiles/views.py
+++ perfiles/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import UpdateView
 
 
-from perfiles.forms import UserRegisterForm, UserUpdateForm
+from perfiles.forms import *
 
 #registro
 def registro(request):
@@ -66,3 +66,23 @@ class MiPerfilUpdateView(LoginRequiredMixin, UpdateView):
 
    def get_object(self, queryset=None):
        return self.request.user
+
+#avatar
+
+def agregar_avatar(request):
+  if request.method == "POST":
+      formulario = AvatarFormulario(request.POST, request.FILES) # Aquí me llega toda la info del formulario html
+
+      if formulario.is_valid():
+          avatar = formulario.save()
+          avatar.user = request.user
+          avatar.save()
+          url_exitosa = reverse('inicio')
+          return redirect(url_exitosa)
+  else:  # GET
+      formulario = AvatarFormulario()
+  return render(
+      request=request,
+      template_name="perfiles/formulario_avatar.html",
+      context={'form': formulario},
+  )
